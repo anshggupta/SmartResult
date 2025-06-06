13a4f5bf2a68b03df2888f612cbd64bc54ab8928
anshggupta <anshgggupta300@gmail.com>
2025-06-06 12:09:46 +0530

    update

--- src/main/java/com/smartResult/SmartResultApplication.java
+++ src/main/java/com/smartResult/SmartResultApplication.java
@@ -1,4 +1,4 @@
-package com.SmartResult.SmartResult;
+package com.SmartResult.smartResult;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
